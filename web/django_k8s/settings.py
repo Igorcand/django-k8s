91518b3ac393b3567112f01e87be2accb7b03f8f
Igor Candido <igorcr39@gmail.com>
2023-08-01 06:23:37 -0300

    configured env viriables in settings and configured databases

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2i+!v$pjhjv0^e-i%al-3c7$72uk#2$%*-11pd1dt$4w!exi25'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 ALLOWED_HOSTS = []
 
@@ -80,7 +82,33 @@ DATABASES = {
     }
 }
 
+DB_USERNAME = os.environ.get('POSTEGRES_USER')
+DB_PASSWORD = os.environ.get('POSTEGRES_PASSWORD')
+DB_DATABASE = os.environ.get('POSTEGRES_DB')
+DB_HOST = os.environ.get('POSTEGRES_HOST')
+DB_PORT = os.environ.get('POSTEGRES_PORT')
+DB_IS_AVAIL = all([
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_DATABASE,
+    DB_HOST,
+    DB_PORT
 
+])
+
+POSTGRES_READY=str(os.environ.get('POSTGRES_READY')) == '1'
+
+if DB_IS_AVAIL and POSTGRES_READY:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_DATABASE,
+        'USER': DB_USERNAME,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
